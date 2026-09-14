ab87546e94ab5ab911625dc949b8e1428da154ab
Yersin <ersinturgin@gmail.com>
2026-09-14 05:18:42 +0500

    practise 1 python basics examples

---
+++ basics 1.py
@@ -0,0 +1,24 @@
+#1 
+print ("My name is Yersin")
+print ("i am a student kbtu")
+#2
+name = "Yersin"
+age = "19"
+print(name)
+print(age)
+#3
+name = "Yersin"
+age = "19"
+height = "177"
+student = True
+
+print(name)
+print(age)
+print(height)
+print(student)
+#4 
+if 5 > 2:
+  print("Five is greater than two!")
+#5
+x = 5
+print(type(x))
