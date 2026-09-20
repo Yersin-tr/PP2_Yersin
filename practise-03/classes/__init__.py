429566934b4c16e18c36e69a365943375499abf4
Yersin <ersinturgin@gmail.com>
2026-09-20 07:23:39 +0500

    update code

--- practise-03/classes/__init__.py
+++ practise-03/classes/__init__.py
@@ -2,3 +2,10 @@ def my_function():
   print("Привет из функции")
 
 my_function()
+#пример 2 Одну и ту же функцию можно вызывать несколько раз:
+def my_function():
+  print("Привет из функции")
+
+my_function()
+my_function()
+my_function()
